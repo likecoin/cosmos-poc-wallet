c4178034d15cebc3ad3bbb2a7fd905fdbd5d2623
Chung Wu <imwmbm@gmail.com>
2019-06-26 22:16:11 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,120 @@
 <template>
   <div id="app">
-    <div class="top-bar">
-      <div>
-        <span class="menu-link"><a href="#validators">Validators</a></span>
-        <span class="menu-link"><a href="#transfer">Transfer</a></span>
-      </div>
-      <div>
-        Mnemonic: <input v-model="mnemonic" />
-        <button @click="commitMnemonic">Submit</button>
-      </div>
-      <div>
-        Don't have mnemonic? Click <button @click="generateMnemonic">here</button> to generate one in the box above
-      </div>
-      <div>
-        Your address: {{ address }}
-      </div>
-      <div>
-        Your balance: {{ balance }}
-      </div>
-    </div>
-    <div class="main">
-      <div class="validators">
-        <h1 id="validators">Validators / Delegation</h1>
-        <div class="validator-cell" v-for="(v, i) in validators" v-bind:key="v.operator_address">
-          <h2>{{ v.description.moniker }}</h2>
-          <div>
-            Address: {{ v.operator_address }}
+    <v-app>
+      <v-toolbar fixed>
+        <v-btn v-if="logined" @click="logout">Logout</v-btn>
+        <v-btn v-else @click="loginDialog = true">Login</v-btn>
+        <v-dialog v-model="loginDialog">
+          <v-card>
+            <v-card-title class="headline">
+              Please enter your mnemonic
+            </v-card-title>
+            <v-card-text>
+              <v-text-field label="mnenomic" v-model="mnemonic"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn @click="commitMnemonic">OK</v-btn>
+              <v-btn @click="loginDialog = false">Cancel</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-spacer></v-spacer>
+        <div v-if="logined">
+          {{ address }} ({{balance}})
+        </div>
+        <v-spacer></v-spacer>
+        <v-btn href="#validators">Validators</v-btn>
+        <v-btn href="#transfer">Transfer</v-btn>
+      </v-toolbar>
+        <!-- <div>
+          Don't have mnemonic? Click <button @click="generateMnemonic">here</button> to generate one in the box above
+        </div> -->
+      <div class="main">
+        <div class="validators">
+          <h1 class="anchor" id="validators">Validators / Delegation</h1>
+          <div class="validator-cell" v-for="(v, i) in validators" v-bind:key="v.operator_address">
+            <h2>{{ v.description.moniker }}</h2>
+            <div>
+              Address: {{ v.operator_address }}
+            </div>
+            <div>
+              Description: {{ v.description.details }}
+            </div>
+            Delegate amount: <input ref="delegateAmount" />
+            <button @click="delegate(v, i)" :disabled="!logined">Delegate</button>
           </div>
-          <div>
-            Description: {{ v.description.details }}
-          </div>
-          Delegate amount: <input ref="delegateAmount" />
-          <button @click="delegate(v, i)" :disabled="!logined">Delegate</button>
+        </div>
+        <div class="transfer">
+          <h1 class="anchor" id="transfer">Transfer</h1>
+          <div>To: <input ref="transferTo"/></div>
+          <div>Value: <input ref="transferValue"/></div>
+          <button @click="transfer()" :disabled="!logined">Transfer</button>
         </div>
       </div>
-      <div class="transfer">
-        <h1 id="transfer">Transfer</h1>
-        <div>To: <input ref="transferTo"/></div>
-        <div>Value: <input ref="transferValue"/></div>
-        <button @click="transfer()" :disabled="!logined">Transfer</button>
+      <div>
+        space
       </div>
-    </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+      <div>
+        space
+      </div>
+    </v-app>
   </div>
 </template>
 
@@ -85,14 +159,18 @@ export default {
       address: "",
       account: {},
       validators: [],
+      loginDialog: false,
     };
   },
   computed: {
     balance() {
-      if (!this.account || !this.account.coins) {
-        return "";
+      if (!this.address) {
+        return "N/A";
       }
-      const { amount } = this.account.coins.filter((coin) => coin.denom === DENOM)[0];
+      let amount = 0;
+      if (this.account && this.account.coins) {
+        amount = this.account.coins.filter((coin) => coin.denom === DENOM)[0].amount;
+      }
       return Number.parseFloat(amount) / 1e9 + " LIKE";
     },
     logined() {
@@ -109,6 +187,7 @@ export default {
     async commitMnemonic() {
       const wallet = getNewWalletFromSeed(this.mnemonic);
       this.mnemonic = "";
+      this.loginDialog = false;
       const publicKey = Buffer.from(wallet.publicKey, "hex");
       const privateKey = Buffer.from(wallet.privateKey, "hex");
       this.signer = (signMessage) => {
@@ -117,6 +196,11 @@ export default {
       }
       this.address = wallet.cosmosAddress;
       this.account = await api.get.account(wallet.cosmosAddress);
+    },
+    logout() {
+      this.signer = null;
+      this.address = "";
+      this.account = {};
     },
     async delegate(validator, i) {
       const from = normalizeAddress(this.address);
@@ -144,19 +228,23 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
+} */
 
-.validator-cell {
+/* .validator-cell {
   border-style: solid;
 }
 
 .menu-link {
   display: inline-block;
   margin: 10px;
+} */
+
+.anchor {
+  padding-top: 65px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify);
+
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
